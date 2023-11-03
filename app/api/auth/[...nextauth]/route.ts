82b53be0d861a43d021e1cce5ea13c0e5c29165c
Jerry Hobby <Jerry@JerryHobby.com>
2023-11-03 17:42:36 -0500

    fix up articles page

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,37 +8,36 @@ import prisma from "@/prisma/client";
 const nextAuthOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                id: {label: "ID", type: "text"},
-                email: {label: "Email", type: "email"},
-                password: {label: "Password", type: "password"}
-            },
-            async authorize(credentials, req) {
-                if (!credentials?.email || !credentials?.password) {
-                    return null;
-                }
-                const user = await prisma.user.findUnique(
-                    {
-                        where: {
-                            email: credentials.email,
-                        }
-                    });
-
-                if (!user) {
-                    return null;
-                }
-
-                const passwordsMatched = await bcrypt.compare(credentials.password, user.hashedPassword!);
-                //const isValid = await prisma.user.validatePassword(credentials.password, user.hashedPassword);
-
-                if (!passwordsMatched) {
-                    return null;
-                }
-                return user;
-            }
-        }),
+        // CredentialsProvider({
+        //     name: "Credentials",
+        //     credentials: {
+        //         email: {label: "Email", type: "email"},
+        //         password: {label: "Password", type: "password"}
+        //     },
+        //     async authorize(credentials, req) {
+        //         if (!credentials?.email || !credentials?.password) {
+        //             return null;
+        //         }
+        //         const user = await prisma.user.findUnique(
+        //             {
+        //                 where: {
+        //                     email: credentials.email,
+        //                 }
+        //             });
+        //
+        //         if (!user) {
+        //             return null;
+        //         }
+        //
+        //         const passwordsMatched = await bcrypt.compare(credentials.password, user.hashedPassword!);
+        //         //const isValid = await prisma.user.validatePassword(credentials.password, user.hashedPassword);
+        //
+        //         if (!passwordsMatched) {
+        //             return null;
+        //         }
+        //         return user;
+        //     }
+        // }),
 
         GoogleProvider({
                 clientId: process.env.GOOGLE_CLIENT_ID!,
