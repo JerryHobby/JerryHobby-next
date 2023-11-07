639570fb3040c9a3ba255fe268e72458ebe2d064
Jerry Hobby <Jerry@JerryHobby.com>
2023-11-07 16:14:34 -0600

    airports loader test

--- airport_data_loader.sh
+++ airport_data_loader.sh
@@ -3,18 +3,18 @@ TARGETDIR=/tmp/airport_data
 mkdir -p $TARGETDIR
 
 URL=https://raw.githubusercontent.com/davidmegginson/ourairports-data/main/
-FILES=(airports.csv runways.csv navaids.csv airport-frequencies.csv countries.csv regions.csv)
+#FILES=(airports.csv runways.csv navaids.csv airport-frequencies.csv countries.csv regions.csv)
 INFILES=(runways.csv navaids.csv)
 OUTFILES=(Runways.csv Navaids.csv)
 
 
-for (( i = 1; i <= $#FILES; i++ )) do
+for (( i = 1; i <= $#INFILES; i++ )) do
     ( # subshell to contain the effect of the chdir
         RAW=$TARGETDIR/in-"${INFILES[i]}"
         CSV=$TARGETDIR/"${OUTFILES[i]}"
 
         echo "#############################################"
-        curl $URL"${FILES[i]}" > "$RAW"
+        curl $URL"${INFILES[i]}" > "$RAW"
         sed 's/,,/,\\N,/g' "$RAW" | sed 's/,,/,\\N,/g' > "$CSV"
         mysqlimport --user=jerryhobby --host='jerryhobby.com' --password='wjb7ETJ*dzr2jeu2wuf' jerryhobby "$CSV" --delete --force --verbose --ignore-lines=1 --fields-terminated-by=',' --fields-enclosed-by='"' --lines-terminated-by='\n'
     )
