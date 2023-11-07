970dfd1a810a27972652283af4d87a64968b0c04
Jerry Hobby <Jerry@JerryHobby.com>
2023-11-07 13:31:09 -0600

    airports tweaks

--- app/airports/searchForm.tsx
+++ app/airports/searchForm.tsx
@@ -31,7 +31,7 @@ const SearchForm = ({search}:Props) => {
 
         <form onSubmit={handleSubmit(onSubmit)}>
             {/* include validation with required or other standard HTML validation rules */}
-            <input defaultValue={find} className='text-sm p-0.5 border fill-accent rounded border-accent-focus' {...register("search", { required: true })} />
+            <input defaultValue={find} className='bg-white p-0.5 border fill-accent rounded border-accent-focus' {...register("search", { required: true })} />
             {/* errors will return when field validation fails  */}
             {errors.exampleRequired && <span>This field is required</span>}
 
