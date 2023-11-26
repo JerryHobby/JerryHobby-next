45c5fb68228a59418c78911f627a1bb846f5d2a6
Jerry Hobby <Jerry@JerryHobby.com>
2023-11-26 12:35:48 -0600

    added kanban image

--- build.sh
+++ build.sh
@@ -1,9 +1,71 @@
 #!/bin/zsh
 
-git pull
-npm i
-npm update
-npx prisma generate client
-npm run build
-pm2 restart JerryHobby.com
+echo "###############################################"
+echo git restore package-lock.json
 
+git restore package-lock.json
+RC=$?
+if [ $RC -ne 0 ]; then
+          echo '\n' $RC was returned.
+          exit
+fi
+
+echo "###############################################"
+echo git pull
+
+git pull
+RC=$?
+if [ $RC -ne 0 ]; then
+          echo '\n' $RC was returned.
+          exit
+fi
+
+echo "###############################################"
+echo npm i
+
+npm i
+RC=$?
+if [ $RC -ne 0 ]; then
+          echo '\n' $RC was returned.
+          exit
+fi
+
+echo "###############################################"
+echo npm update
+
+npm update
+RC=$?
+if [ $RC -ne 0 ]; then
+          echo '\n' $RC was returned.
+          exit
+fi
+
+echo "###############################################"
+echo npx prisma generate client
+
+npx prisma generate client
+RC=$?
+if [ $RC -ne 0 ]; then
+          echo '\n' $RC was returned.
+          exit
+fi
+
+echo "###############################################"
+echo npm run build
+
+npm run build
+RC=$?
+if [ $RC -ne 0 ]; then
+          echo '\n' $RC was returned.
+          exit
+fi
+
+echo "###############################################"
+echo pm2 restart JerryHobby.com
+
+pm2 restart JerryHobby.com
+RC=$?
+if [ $RC -ne 0 ]; then
+          echo '\n' $RC was returned.
+          exit
+fi
