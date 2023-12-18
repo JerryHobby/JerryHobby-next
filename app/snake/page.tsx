131da314c836e6881de983dc020f8f9da60c7329
Jerry Hobby <Jerry@JerryHobby.com>
2023-12-18 15:26:25 -0600

    added snake game - not fully functional yet.

--- app/snake/page.tsx
+++ app/snake/page.tsx
@@ -8,14 +8,14 @@ const Page = () => {
             <div id="snake-game" className="content-wrapper">
                 <h2>Jerry's WASM/Rust Snake Game</h2>
                 <canvas id="snake-canvas"></canvas>
-                <div id="snake-controls">
-                    <button id="snake-start" onClick={() => window.startGame()}>Start</button>
-                    <button id="snake-pause" onClick={() => window.pauseGame()}>Pause</button>
-                    <button id="snake-reset" onClick={() => window.resetGame()}>Reset</button>
-                    <div id="score">Score: <span id="snake-score">0</span></div>
-                </div>
+            {/*    <div id="snake-controls">*/}
+            {/*        <button id="snake-start" onClick={() => window.startGame()}>Start</button>*/}
+            {/*        <button id="snake-pause" onClick={() => window.pauseGame()}>Pause</button>*/}
+            {/*        <button id="snake-reset" onClick={() => window.resetGame()}>Reset</button>*/}
+            {/*        <div id="score">Score: <span id="snake-score">0</span></div>*/}
+            {/*    </div>*/}
             </div>
-            <script src="bootstrap.js"></script>
+            {/*<script src="bootstrap.js"></script>*/}
         </>
     );
 };
