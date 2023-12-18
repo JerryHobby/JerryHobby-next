5729079e81147460098a65d2ff6786874eeb339a
Jerry Hobby <Jerry@JerryHobby.com>
2023-12-18 15:28:33 -0600

    added snake game - not fully functional yet.

--- app/snake/page.tsx
+++ app/snake/page.tsx
@@ -6,7 +6,7 @@ const Page = () => {
     return (
         <>
             <div id="snake-game" className="content-wrapper">
-                <h2>Jerry's WASM/Rust Snake Game</h2>
+                <h2>Jerry&apos;s WASM/Rust Snake Game</h2>
                 <canvas id="snake-canvas"></canvas>
             {/*    <div id="snake-controls">*/}
             {/*        <button id="snake-start" onClick={() => window.startGame()}>Start</button>*/}
